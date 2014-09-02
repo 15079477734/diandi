9035ca1128793ccad1ece0d419a5109d979e77ec
15079477734 <462679107@qq.com>
2014-09-02 18:38:11 +0800

    1.1

---
+++ app/src/main/java/com/bmob/im/demo/test.java
@@ -0,0 +1,11 @@
+package com.bmob.im.demo;
+
+/**
+ * Created by sulinger  .
+ * date:14-9-2 .
+ * time:18:37 .
+ * project:BmobChatDemo .
+ * Copyright © sulinger .All Rights Reserved.
+ */
+public class test {
+}
