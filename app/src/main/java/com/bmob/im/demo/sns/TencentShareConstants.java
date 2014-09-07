12f7c613d54e0f3b6615a1530a102bbf993b5343
klob <185902156@qq.com>
2014-09-08 00:06:59 +0800

    3.0

--- app/src/main/java/com/bmob/im/demo/sns/TencentShareConstants.java
+++ app/src/main/java/com/bmob/im/demo/sns/TencentShareConstants.java
@@ -7,13 +7,13 @@ package com.bmob.im.demo.sns;
 
 public interface TencentShareConstants {
 
-    String TITLE="推荐一个好应用哦~";
+    String TITLE = "推荐一个好应用哦~";
 
-    String IMG_URL="http://t2.qpic.cn/mblogpic/357eac0bdbb308df7c30/120";
+    String IMG_URL = "http://t2.qpic.cn/mblogpic/357eac0bdbb308df7c30/120";
 
-    String TARGET_URL="http://yuanquan.bmob.cn";
+    String TARGET_URL = "http://mydata123.oss-cn-hangzhou.aliyuncs.com/%E7%82%B9%E6%BB%B4%E5%8F%91%E5%B8%83/%E7%82%B9%E6%BB%B41.0.apk";
 
-    String SUMMARY="专业小编团队每日推荐平价又时尚的宝贝~宝贝超赞，还包邮哦~";
+    String SUMMARY = "专业小编团队每日推荐平价又时尚的宝贝~宝贝超赞，还包邮哦~";
 
-    String COMMENT="";
+    String COMMENT = "";
 }
